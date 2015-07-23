15698bd4e9c4eaebea85aac40d8f411cc85c4919
Corbin Otwell <cbotwell@gmail.com>
2015-07-23 14:24:34 -0500

    default files, some boilerplate, and build tools

---
+++ Brocfile.js
@@ -0,0 +1,22 @@
+var merge = require('broccoli-merge-trees');
+var sass = require('broccoli-sass');
+var funnel = require('broccoli-funnel');
+var injectLivereload = require('broccoli-inject-livereload');
+
+var publicWithReload = injectLivereload('public');
+
+var includePaths = [
+    'assets/scss',
+    'bower_components/reset-css',
+    'bower_components/bourbon/app/assets/stylesheets',
+    'bower_components/fontawesome/scss',
+    'bower_components/neat/app/assets/stylesheets'
+];
+
+var vendor = funnel('bower_components', {
+    files: ['jquery/dist/jquery.min.js']
+});
+
+var compiledCss = sass (includePaths, 'accordian.scss', 'accordian.css');
+
+module.exports = merge([publicWithReload, 'assets/js', compiledCss, vendor]);
