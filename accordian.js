fa854d344d761fd953876a177b09060fd98f1760
Corbin Otwell <cbotwell@gmail.com>
2015-07-24 10:31:34 -0500

    deploying

---
+++ accordian.js
@@ -0,0 +1,8 @@
+var topItemEl = $('.top-item');
+
+topItemEl.on('click', function() {
+  $(this).children('button').toggleClass('hulk');
+  $(this).children('.sub-menu').slideToggle('slow');
+  $(this).siblings().find('button').removeClass('hulk');
+  $(this).siblings().find('.sub-menu').slideUp('slow');
+});
