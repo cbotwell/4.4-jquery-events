48b997283f182770901452f00b5e1910233df71b
Corbin Otwell <cbotwell@gmail.com>
2015-07-24 09:29:55 -0500

    tweaked a few things to make it work for $(this)

--- assets/js/accordian.js
+++ assets/js/accordian.js
@@ -2,7 +2,8 @@ var topItemEl = $('.top-item');
 var subEl = $('li.sub');
 
 topItemEl.on('click', function() {
-  topItemEl.addClass('hulk');
-  subEl.slideDown('slow', function() {
+  var id = $(this).data('opens');
+  $(this).addClass('hulk');
+  $('#' + id).slideDown('slow', function() {
   });
 });
