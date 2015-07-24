737faa202d45784f0ea8f729917b6561a94b157d
Corbin Otwell <cbotwell@gmail.com>
2015-07-24 14:55:27 -0500

    got most of keys working except delete, caps, shift, return

--- assets/js/keyboard.js
+++ assets/js/keyboard.js
@@ -1,5 +1,19 @@
 var buttonEl = $('button');
+var textArea = $('textarea');
+
+textArea.innerHTML = '';
 
 buttonEl.on('click', function() {
-  // code for adding to textare here
+  if (this.innerHTML === 'tab') {
+    textArea.append('  ');
+  } else if (this.innerHTML === '') {
+    textArea.append(' ');
+  } else if (this.innerHTML === 'delete') {
+    var newText = textArea.slice(0, -1);
+    textArea.innerHTML = newText;
+  } else if (this.innerHTML === 'caps lock' || this.innerHTML === 'shift' || this.innerHTML === 'return') {
+    return false;
+  } else {
+    textArea.append(this.innerHTML);
+  }
 });
